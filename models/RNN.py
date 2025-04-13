c89047745194e3e0134a1e570d32ce81250e3a58
yashasnadigsyn <yashasnadigsyn@protonmail.com>
2025-04-13 17:44:03 +0530

    Added RNNs

---
+++ models/RNN.py
@@ -0,0 +1,90 @@
+import math
+import torch
+from torch import nn
+from torch.nn import functional as F
+from d2l import torch as d2l
+from data_utils import TrumpTranscript
+
+## The RNNScratch and RNNLMScratch is copied from my own notes which itself is 
+## from the book d2l.ai
+
+class RNNScratch(d2l.Module):
+    """The RNN model implemented from scratch."""
+    def __init__(self, num_inputs, num_hiddens, sigma=0.01):
+        super().__init__()
+        self.save_hyperparameters()
+        self.W_xh = nn.Parameter(
+            torch.randn(num_inputs, num_hiddens) * sigma)
+        self.W_hh = nn.Parameter(
+            torch.randn(num_hiddens, num_hiddens) * sigma)
+        self.b_h = nn.Parameter(torch.zeros(num_hiddens))
+
+    def forward(self, inputs, state=None):
+        if state is None:
+        # Initial state with shape: (batch_size, num_hiddens)
+            state = torch.zeros((inputs.shape[1], self.num_hiddens), device=inputs.device)
+        else:
+            state, = state
+        outputs = []
+        for X in inputs:  # Shape of inputs: (num_steps, batch_size, num_inputs)
+            state = torch.tanh(torch.matmul(X, self.W_xh) +
+                         torch.matmul(state, self.W_hh) + self.b_h)
+            outputs.append(state)
+        return outputs, state
+    
+
+class RNNLMScratch(d2l.Classifier): 
+    """The RNN-based language model implemented from scratch."""
+    def __init__(self, rnn, vocab_size, lr=0.01):
+        super().__init__()
+        self.save_hyperparameters()
+        self.init_params()
+
+    def init_params(self):
+        self.W_hq = nn.Parameter(
+            torch.randn(
+                self.rnn.num_hiddens, self.vocab_size) * self.rnn.sigma)
+        self.b_q = nn.Parameter(torch.zeros(self.vocab_size))
+
+    def training_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('ppl', torch.exp(l), train=True)
+        return l
+
+    def validation_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('ppl', torch.exp(l), train=False)
+
+    def one_hot(self, X):
+        # Output shape: (num_steps, batch_size, vocab_size)
+        return F.one_hot(X.T, self.vocab_size).type(torch.float32)
+    
+    def output_layer(self, rnn_outputs):
+        outputs = [torch.matmul(H, self.W_hq) + self.b_q for H in rnn_outputs]
+        return torch.stack(outputs, 1)
+
+    def forward(self, X, state=None):
+        embs = self.one_hot(X)
+        rnn_outputs, _ = self.rnn(embs, state)
+        return self.output_layer(rnn_outputs)
+    
+    def predict(self, prefix, num_preds, vocab, device=None):
+        state, outputs = None, [vocab[prefix[0]]]
+        for i in range(len(prefix) + num_preds - 1):
+            X = torch.tensor([[outputs[-1]]], device=device)
+            embs = self.one_hot(X)
+            rnn_outputs, state = self.rnn(embs, state)
+            if i < len(prefix) - 1:  # Warm-up period
+                outputs.append(vocab[prefix[i + 1]])
+            else:  # Predict num_preds steps
+                Y = self.output_layer(rnn_outputs)
+                outputs.append(int(Y.argmax(axis=2).reshape(1)))
+        return ''.join([vocab.idx_to_token[i] for i in outputs])
+
+    
+data = TrumpTranscript(batch_size=1024, num_steps=32)
+rnn = RNNScratch(num_inputs=len(data.vocab), num_hiddens=32)
+model = RNNLMScratch(rnn, vocab_size=len(data.vocab), lr=1)
+trainer = d2l.Trainer(max_epochs=100, gradient_clip_val=1, num_gpus=1)
+trainer.fit(model, data)
+model.predict('make america', 20, data.vocab, d2l.try_gpu())
